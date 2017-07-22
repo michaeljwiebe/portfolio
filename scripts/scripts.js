87fdd8e938ad063323370ec2ca2a5b66b73e0883
Michael J. Wiebe <wiebe.michael.j@gmail.com>
2017-07-21 23:00:23 -0400

    overlays on skills in place

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,7 @@
 var projectLinks = document.getElementsByClassName("project-link");
+var skillsLists = document.getElementsByClassName("skills-list");
+var skillCategories = document.getElementsByClassName("skill-category");
+var skills = document.getElementsByClassName("skill");
 
 [].forEach.call(projectLinks, function(link) {
 	link.addEventListener("mouseenter", function() {
@@ -6,5 +9,16 @@ var projectLinks = document.getElementsByClassName("project-link");
 	});
 	link.addEventListener("mouseleave", function() {
 		link.classList.remove("project-link-animation");
+	});
+});
+
+[].forEach.call(skills, function(skill, index) {
+	skill.addEventListener("mouseenter", function() {
+		skillCategories[index].classList.add("skill-category-overlay");
+		skillsLists[index].classList.add("fade-out");
+	});
+	skill.addEventListener("mouseleave", function() {
+		skillCategories[index].classList.remove("skill-category-overlay");
+		skillsLists[index].classList.remove("fade-out");
 	});
 });
